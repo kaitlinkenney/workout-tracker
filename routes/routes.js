398905e297deb2faff2cfde0c202201a2c7047d1
kaitlinkenney <kaitlin.kenney96@gmail.com>
2021-01-08 17:29:39 -0500

    exercises adding

--- routes/routes.js
+++ routes/routes.js
@@ -14,8 +14,16 @@ const Workout = require("../models/workout");
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
+  router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+
   router.get("/api/workouts", (req, res) => {
-    db.Workout.find({})////////////////?
+    Workout.find({})////////////////?
       .then(dbWork => {
         res.json(dbWork);
       })
@@ -25,28 +33,29 @@ const Workout = require("../models/workout");
   });
   
 
-  // app.post("/api/workouts/:id", (req, res) => {
-  //     const newWorkout = {...req.body, day: Date.now()}
+  router.put("/api/workouts/:id", (req, res) => {
+      const newWorkout = {...req.body, day: Date.now()}
 
-  //     Workout.findOneAndUpdate(
-  //       {
-  //           _id: mongojs.ObjectId(req.params.id)
-  //       },
-  //       {
-  //           $set: newWorkout
-  //       },
-  //       (error, data) => {
-  //           if (error) {
-  //             res.send(error);
-  //           } else {
-  //             res.send(data);
-  //           }
-  //         }
-  //     )
-  // })
+      Workout.findOneAndUpdate(
+        {
+            _id: req.params.id
+        },
+        {
+            $set: newWorkout
+        },
+        (error, data) => {
+            if (error) {
+              res.send(error);
+            } else {
+              res.send(data);
+            }
+          }
+      )
+  })
 
-  router.post("/api/workouts", (req, res) => {
-    db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true })
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+    // .then(({ _id }) => Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
       .then(dbWork => {
         res.json(dbWork);
       })
@@ -55,25 +64,29 @@ const Workout = require("../models/workout");
       });
   });
 
-router.post("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then(dbWork => {
-      res.send(dbWork);
-    })
-    .catch(err => {
-      res.send(err);
-    });
-});
+// router.put("/api/workouts/:id", (req, res) {
 
-router.get("/api/workouts/range", (req, res) => {
-  db.Workout.find({})
-      .then(dbWork => {
-        res.json(dbWork);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-})
+// })
+
+// router.post("/api/workouts", ({ body }, res) => {
+//   Workout.create(body)
+//     .then(dbWork => {
+//       res.send(dbWork);
+//     })
+//     .catch(err => {
+//       res.send(err);
+//     });
+// });
+
+// router.get("/api/workouts/range", (req, res) => {
+//   Workout.find({})
+//       .then(dbWork => {
+//         res.json(dbWork);
+//       })
+//       .catch(err => {
+//         res.json(err);
+//       });
+// })
 
 // }
 
